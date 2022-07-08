e47e8bd1b4e2af4ab736bd7533318bb6c50790d6
Mason <70880906+MasonGYC@users.noreply.github.com>
2022-07-08 23:14:24 +0800

    edited readme

--- CompareFileTest.java
+++ CompareFileTest.java
@@ -1,16 +1,29 @@
 import static org.junit.Assert.*;
 import java.io.File;
 import org.junit.Test;
+import java.nio.file.Paths;
+
+
 
 public class CompareFileTest { 
    @Test
    public void Compare () throws Exception { 
+
+    // convert file names to absolute paths
+    String cwd = "D:/DataReconcilation/"; // replace with files' dir path
+    String filein1 = cwd + "sample_file_1.csv";
+    String filein2 = cwd + "sample_file_3.csv";
+    String fileout1 = cwd + "compare_out.csv";
+    String fileout2 = cwd + "test_out.csv";
+    String fileAnswer = cwd + "sample_file_output_comparing_1_and_3.csv";
+
+    // test
     CompareFile a = new CompareFile(); 
-    String[] filenames = {"D:/DataReconcilation/sample_file_1.csv","D:/DataReconcilation/sample_file_3.csv","D:/DataReconcilation/compare_out.csv"};
+    String[] filenames = {filein1,filein2,fileout1};
     a.compare(filenames);
-    String[] filenamesTest = {"D:/DataReconcilation/compare_out.csv","D:/DataReconcilation/sample_file_output_comparing_1_and_3.csv","D:/DataReconcilation/test_out.csv"};
+    String[] filenamesTest = {fileout1,fileAnswer,fileout2};
     a.compare(filenamesTest);
-    File test_out = new File("D:/DataReconcilation/test_out.csv");
+    File test_out = new File(fileout2);
     assertEquals(0, test_out.length());
 
 
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# DataReconcilation
+# Data Reconcilation
 Guo Yuchen 1004885
+
+This program is for comparing 2 csv files and record their difference in a new csv file.
+
+### Getting Started (Installation)
+
+1. Clone/download the repository to your computer;
+2. Change `cwd` inside `CompareFileTest.java` `compare` function with test files' dir
+3. Compile and run `CompareFile.java`, check `compare_out.csv` for the differences detected in 2 input files;
+4. Test using `CompareFileTest.java`, should be able to pass the test. (download JUnit before testing).
